aea939b82fd0555abf313d28182f7d36f84cea60
Robbe Van Herck <robbevanherck1@gmail.com>
2019-08-23 09:42:18 +0200

    Round percentages in template

--- jhdag2019.py
+++ jhdag2019.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 parties = {
-        "Partij a": 25,
+        "Partij a": 7,
         "Partij b": 75
 }
 
 @app.route('/')
 def root():
-    return render_template("index.html", parties=parties)
+    total = sum(parties.values())
+    return render_template("index.html", parties=parties, total=total)
