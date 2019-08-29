fb765f1de6eaab2dc7dd34e89cf8deb9e5482634
Robbe Van Herck <robbevanherck1@gmail.com>
2019-08-29 10:04:37 +0200

    Add messages

--- init_db.py
+++ init_db.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
-from jhdag2019 import db, Party
+from jhdag2019 import db, Party, Message
 
 db.drop_all()
 
 db.create_all()
 
-party_1 = Party(name="Zeus WPI", score=10)
-party_2 = Party(name="WiNA", score=1)
+party_1 = Party(name="Partij A", score=10)
+party_2 = Party(name="Partij B", score=1)
+
+message_1 = Message(title="Titel", content="Dit is een bericht met een boodschap en kan misschien wel iets belangrijk bevatten maar nu moet ik gewoon een lange text hebben die het scherm een beetje opvult dus typ ik hier maar wat bij aan...") 
 
 db.session.add(party_1)
 db.session.add(party_2)
+db.session.add(message_1)
 db.session.commit()
--- jhdag2019.py
+++ jhdag2019.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+import datetime
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
@@ -14,13 +15,25 @@ class Party(db.Model):
     score = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        return f"<Party {id}> {name}: {score}"
+        return f"<Party {self.id}> {self.name}: {self.score}"
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(32), nullable=False)
+    content = db.Column(db.String(1024), nullable=False)
+    created_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Party {self.id}> {self.title}: {self.content[:32]}"
 
 admin = Admin(app, name='Jonge Heldendag 2019', template_mode='bootstrap3')
 admin.add_view(ModelView(Party, db.session))
+admin.add_view(ModelView(Message, db.session))
 
 @app.route('/')
 def root():
     parties = Party.query.all()
     total = sum([p.score for p in parties])
-    return render_template("index.html", parties=parties, total=total)
+    messages = Message.query.order_by(Message.created_date.desc()).all()
+    print(messages)
+    return render_template("index.html", parties=parties, total=total, messages=messages)
